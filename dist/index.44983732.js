a2b81540f667821dd2f1f11bab2dccb6c0e16378
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-06-25 09:44:51 +0500

    twitch clone

--- dist/index.44983732.js
+++
@@ -1,9 +0,0 @@
-let container = document.querySelector(".containerP");
-let togle = document.querySelector(".nav_item_two");
-togle.addEventListener("click", function() {
-    container.classList.toggle("active");
-    if (container.classList.contains("active")) container.style.setProperty("--back", "#ACC8E5");
-    else container.style.setProperty("--back", "white");
-});
-
-//# sourceMappingURL=index.44983732.js.map
